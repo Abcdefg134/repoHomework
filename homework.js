c4bccf5b8806e665e143cc1451c42dd582674d85
abcdefg134 <toan198vn1@gmail.com>
2021-09-28 15:19:40 +0700

    Update homework.js

--- homework.js
+++ homework.js
@@ -46,3 +46,25 @@ function largestOfFour(arr) {
   }
   
   largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+// Confirm the Ending
+
+function confirmEnding(str, target) {
+  return str.slice(str.length - target.length) === target
+}
+
+confirmEnding("Bastian", "n");
+
+// Repeat a String Repeat a String
+
+function repeatStringNumTimes(str, num) {
+  if(num<1){
+    return ''
+  }
+  for(var a = '',i=num; i>0;i--){
+    a += str
+  }
+  return a;
+}
+
+repeatStringNumTimes("abc", 0); //''
+repeatStringNumTimes('abc',3);// abcabcabc
