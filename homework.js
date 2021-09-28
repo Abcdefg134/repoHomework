2ae7e139db4a3b8446eb862b4a4f743fdde80eb3
abcdefg134 <toan198vn1@gmail.com>
2021-09-28 09:11:14 +0700

    Create homework.js

---
+++ homework.js
@@ -0,0 +1,48 @@
+// Convert Celsius to FahrenheitPassed
+
+function convertToF(celsius) {
+    let fahrenheit = celsius* 9/5 + 32
+    return fahrenheit;
+  }
+  
+  convertToF(30);
+
+//Reverse a String
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+  }
+  
+  reverseString("hello");
+
+//Factorialize a Number
+
+function factorialize(num) {
+    if (num === 0) {
+      return 1;
+    }
+    console.log(num * factorialize(num - 1));
+    return num * factorialize(num - 1);
+  }
+  
+factorialize(5);
+
+//Find the Longest Word in a String
+function findLongestWordLength(str) {
+    let a = str.split(' ').map(x=>x.length)
+    return Math.max(...a);
+  }
+  
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+//Return Largest Numbers in Arrays
+
+function largestOfFour(arr) {
+    return arr.map((subArray)=> {
+      return subArray.reduce((prev, current)=> {
+        return current > prev ? current : prev;
+      });
+    });
+  }
+  
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
