c8c744cf90064d4cdeb2f3ab10e78c25c4d82776
abcdefg134 <toan198vn1@gmail.com>
2021-09-29 07:38:20 +0700

    Update homework.js

--- homework.js
+++ homework.js
@@ -103,3 +103,70 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
+// Slice and Splice
+
+function frankenSplice(arr1, arr2, n) {
+  let arr = arr2.slice()
+  for (let i = 0; i < arr1.length; i++) {
+    arr.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return arr;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+// Falsy Bouncer
+
+function bouncer(arr) {
+  let newArr = []
+  for(let i = 0; i<arr.length;i++){
+    arr[i]? newArr.push(arr[i]): null
+  }
+  return newArr;
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+//Where do I Belong
+
+function getIndexToIns(arr, num) {
+  return arr.filter(val => num > val).length;
+}
+
+getIndexToIns([40, 60], 50);
+
+// Mutations
+
+function mutation(arr) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
+
+mutation(["hello", "hey"]);
+
+//
+
+function chunkArrayInGroups(arr, size) {
+  let temp = [];
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i % size !== size - 1) temp.push(arr[i]);
+    else {
+      temp.push(arr[i]);
+      result.push(temp);
+      temp = [];
+    }
+  }
+
+  if (temp.length !== 0) result.push(temp);
+  return result;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
